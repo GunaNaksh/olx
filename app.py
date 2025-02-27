70a9ce3341a74601a81717fcb5f47f661b4d9202
Guna Naksh <guna.ravi7@gmail.com>
2025-02-28 01:53:39 +0530

    hi

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def homepage():
     return render_template("home.html")
 
-
+@app.route('/contact')
+def contactus():
+    return render_template("contactus.html")
 
 
 if __name__=='__main__':
